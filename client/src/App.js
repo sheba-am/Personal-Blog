b4d9343adf49e53303014ef4400cc4d4d1217308
sheba-am <saba.ameri1378@gmail.com>
2023-11-04 17:36:40 +0330

    some css for login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,42 +2,66 @@ import './App.css';
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet,
 } from "react-router-dom";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Single from "./pages/Single";
 import Write from "./pages/Write";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './style.scss';
+
+const Layout = () => {
+  return(
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  );
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },  {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
   {
     path: "/register",
     element: <Register />,
   },
+
   {
-    path: "/home",
-    element: <Home />,
-  },  {
     path: "/login",
     element: <Login />,
-  },  {
-    path: "/single",
-    element: <Single />,
-  },  {
-    path: "/write",
-    element: <Write />,
-  },
+  },  
+
 ]);
 function App() {
   return (
-    <div className="App">
-          <RouterProvider router={router} />
+    <div className="app">
+          <div className='container'>
+            <RouterProvider router={router} />
+          </div>
 
     </div>
   );
 }
+
 
 export default App;
