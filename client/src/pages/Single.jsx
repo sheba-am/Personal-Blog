e322eb863f65965489c83043f6f571f0ff384cd7
sheba-am <saba.ameri1378@gmail.com>
2023-11-06 11:55:19 +0330

    single post page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+import { Link } from 'react-router-dom'
+import Menu from '../components/Menu'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className='content'>
+        <img src= "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt='' />
+        <div className='user'>
+          <img src= "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt='' />
+          <div className='info'>
+          <span> John</span>
+          <p> Posted 2 days ago</p>
+          </div>
+          <div className='edit' >
+            <Link to={'/write?edit=2'}><img src={Edit} alt=''/></Link>
+            <img src={Delete} alt='' />
+          </div>
+        </div>
+        <h1> Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!</h1>
+        <p>"Text: Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!""Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!""Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!"Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!</p>
+
+      </div>
+    <Menu />
+      
+    </div>
   )
 }
 
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Write = () => {
   return (
-    <div>Write</div>
+    <div className='add'>
+      <div className="content">content</div>
+      <div className="menu">
+        <div className="item">i1</div>
+        <div className="item">i2</div>
+      </div>
+    </div>
   )
 }
 
