0495101f0c68829a8a27effb2d298d5b934ed1f4
sheba-am <saba.ameri1378@gmail.com>
2023-11-08 12:16:13 +0330

    creating context for user

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,19 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context/authContext'
 
 const Login = () => {
   const [inputs, setInputs] = useState({
     username: "",
     password: "",
-  })
+  }); 
 
-  const [err, setError] = useState(null)
-  const navigate = useNavigate()
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const {login} = useContext(AuthContext);
 
   const handleChange = e => {
     setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
@@ -18,7 +22,7 @@ const Login = () => {
   const handleSubmit = async e => {
     e.preventDefault()
     try {
-      await axios.post("/auth/login", inputs) 
+      await login(inputs)
       navigate("/")
     } catch(err) {
       console.log(err)
