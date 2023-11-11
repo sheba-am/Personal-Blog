0553ce20a485092697d5b8dd07f091f070a1404b
sheba-am <saba.ameri1378@gmail.com>
2023-11-11 10:23:07 +0330

    make navbar dynamic

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Logo from '../img/logo.png'
 import {Link} from 'react-router-dom'
+import { AuthContext } from '../context/authContext';
 
 const Navbar = () => {
+  const {currentUser, logout} = useContext(AuthContext);
   return (
     <div className='navbar'>
       <div className='container'>
@@ -14,8 +16,8 @@ const Navbar = () => {
         <Link className='link' to="/?cat=cinema"><h6>CINEMA</h6></Link>
         <Link className='link' to="/?cat=design"><h6>DESIGN</h6></Link>
         <Link className='link' to="/?cat=food"><h6>FOOD</h6></Link>
-        <span> Saba</span>
-        <span> Logout</span>
+        <span> {currentUser?.username}</span>
+        {currentUser? (<span onClick={logout}> Logout</span>):( <Link className='link' to='/login'>Login</Link>)}
         <span className='write'>
           <Link className='link' to="/write">Write</Link>
         </span>
