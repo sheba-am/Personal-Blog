d98d214b22b48a86a769b6e29847ce260e18c5eb
sheba-am <saba.ameri1378@gmail.com>
2023-12-12 11:58:42 +0330

    add pagination to blog

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,65 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import axios from "axios"
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 const Blog = () => {
-  const [posts, setPosts] = useState([])
-  const cat = useLocation().search
-  
-  useEffect(()=> {
-    const fetchData = async() => {
-      try{
-        const res = await axios.get(`/posts${cat}`)
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(5); // Adjust the number of posts per page as needed
+  const cat = useLocation().search;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts${cat}`);
         const sortedPosts = res.data.sort((a, b) => new Date(b.date) - new Date(a.date));
         setPosts(sortedPosts);
-        //setPosts(res.data)
-      }catch(err) {
-        console.log(err)        
+      } catch (err) {
+        console.log(err);
       }
     };
-    fetchData(); // can't make async function with useEffect  so we create function like this to be async
-  }, [cat]) // eveytime we change category fetchData is going to be called again
+    fetchData();
+  }, [cat]);
 
-
-
-  // when getting text don't get the p tag as well
   const getText = (html) => {
-    const doc= new DOMParser().parseFromString(html, "text/html")
-    return doc.body.textContent
-  }
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent;
+  };
+
+  // Calculate the indexes of the posts to be displayed on the current page
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page and scroll to the top
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   return (
     <div className='blog'>
-
-
-      
       <div className='posts'>
-        {posts.map((post) => (
-          //console.log(post.id)
+        {currentPosts.map((post) => (
           <div className='post' key={post.id}>
             <div className='img'>
-              <img  src={`../upload/${post?.img}`} alt=""  onError={(e) => {
-                    e.target.onerror = null; // Prevent infinite loop
-                    e.target.style.display = 'none'; // Hide the image if not found
-                    //e.target.src = '../upload/default-image.png'; // Replace with the path to your default image or a placeholder
-                  }} />
+              <img
+                src={`../upload/${post?.img}`}
+                alt=""
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.style.display = 'none';
+                }}
+              />
             </div>
-            <div className="content">
-              <Link to={`/post/${post.id}`} >
-                <h1>{post.title} </h1>
+            <div className='content'>
+              <Link to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
               </Link>
               <p>{getText(post.desc)}</p>
-              <Link  className="read-more" to={`/post/${post.id}`} >
-                Read more 
+              <Link className='read-more' to={`/post/${post.id}`}>
+                Read more
               </Link>
-              
             </div>
           </div>
         ))}
       </div>
-      
+      {/* Pagination */}
+      <div className='pagination'>
+        {Array.from({ length: Math.ceil(posts.length / postsPerPage) }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => paginate(index + 1)}
+            className={currentPage === index + 1 ? 'current-page-button' : 'page-button'}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
